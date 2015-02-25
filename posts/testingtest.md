895926de26208b103cf13e2178cca47fb53b0407
Sonia Brahmi <sonia.brahmi@lightspeedpos.com>
2015-02-24 23:35:04 -0500

    setting up backend

---
+++ posts/testingtest.md
@@ -0,0 +1,9 @@
+{{{
+  "title" : "Test first Post",
+  "tags" : ["test"],
+  "category" : "testing test",
+  "date" : "2-24-2015",
+  "description" : "setting up blog back end"
+}}}
+
+***Hello World!***
--- server.js
+++ server.js
@@ -3,12 +3,27 @@ var express = require("express"),
     bodyParser = require('body-parser'),
     errorHandler = require('errorhandler'),
     methodOverride = require('method-override'),
+    Poet = require('poet'),
+    dustjs = require('adaro'),
     hostname = process.env.HOSTNAME || 'localhost',
     port = parseInt(process.env.PORT, 10) || 4567,
     publicDir = process.argv[2] || __dirname + '/public';
 
+var poet = Poet(app, {
+    posts: './posts/',
+    postsPerPage: 5,
+    metaFormat: 'json'
+});
+
+poet.watch().init();
+
+app.engine('dust', dustjs.dust({cache: false}));
+app.set('view engine', 'dust');
+
 app.get("/", function (req, res) {
-  res.redirect("/index.html");
+    var posts = app.locals.getPosts();
+    res.locals = {name: 'Sonia', posts: posts};
+    res.render("index");
 });
 
 app.use(methodOverride());
